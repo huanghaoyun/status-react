732abf8ce2c20456158ad3e61df3a48dc74ecd22
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2018-08-13 12:51:01 +0300

    Custom fonts loading added to desktop app

--- src/status_im/ui/screens/desktop/main/chat/views.cljs
+++ src/status_im/ui/screens/desktop/main/chat/views.cljs
@@ -164,6 +164,7 @@
                           :multiline      true
                           :blur-on-submit true
                           :style          styles/chat-text-input
+                          :font           :default
                           :ref            #(reset! inp-ref %)
                           :on-key-press   (fn [e]
                                             (let [native-event (.-nativeEvent e)
